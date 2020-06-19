d55ece593dfc842dd04d7cfebb79fc06e2f64833
Swapnil Banga <swapnil.galaxy@gmail.com>
2020-06-19 23:39:58 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require('./schemas/schema');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect(
+  'mongodb://swapnil:root123@ds043917.mlab.com:43917/graphql-tut',
+  { useNewUrlParser: true, useUnifiedTopology: true }
+)
+
+mongoose.connection.once('open', () => {
+  console.log('connected to db')
+})
+
+app.use("/graphql", graphqlHTTP({ schema: schema, graphiql: true }));
+
+app.listen(5000, () => {
+  console.log("Listening on 5000");
+});
