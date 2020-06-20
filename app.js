9b74fd1d17ce9cb80df378c2c06228883a315176
Swapnil Banga <swapnil.galaxy@gmail.com>
2020-06-20 23:58:51 +0530

    add cors handling

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
-const schema = require('./schemas/schema');
-const mongoose = require('mongoose');
+const schema = require("./schemas/schema");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
-mongoose.connect(
-  'mongodb://swapnil:root123@ds043917.mlab.com:43917/graphql-tut',
-  { useNewUrlParser: true, useUnifiedTopology: true }
-)
+app.use(cors());
 
-mongoose.connection.once('open', () => {
-  console.log('connected to db')
-})
+mongoose.connect(
+  "mongodb://swapnil:root123@ds043917.mlab.com:43917/graphql-tut",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+mongoose.connection.once("open", () => {
+  console.log("connected to db");
+});
 
 app.use("/graphql", graphqlHTTP({ schema: schema, graphiql: true }));
 
